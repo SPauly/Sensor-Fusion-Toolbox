c47b008851ab864b1035ef132eb16794256d13c9
SPauly <spau@hotmail.de>
2025-05-09 02:12:18 +0200

    Full working simulation

--- src/radar_sim.cpp
+++ src/radar_sim.cpp
@@ -2,11 +2,18 @@
 
 #include <stop_token>
 #include <iostream>
+#include <cmath>
 
 #include "sensfus/utils/timers.h"
+#include "sensfus/utils/rand_generator.h"
 
 namespace sensfus {
 namespace sim {
+
+RadarSim::RadarSim() {
+  // (I, 0, 0)
+  H_ << 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0;
+}
 
 void RadarSim::Init() {
   // Init all the simulation data to zero
@@ -78,23 +85,48 @@ void RadarSim::RunImpl() {
   for (size_t i = 0; i < trajectories_.size(); i++) {
     if (trajectories_.at(i).GetSize() >=
         curr_index_ - traj_index_offset_.at(i)) {
+      // For later use it is good to extract the target position here
+      ObjectState2D target_pos =
+          trajectories_.at(i).GetState(curr_index_ - traj_index_offset_.at(i));
+
       // Update the true trajectory position -> account for the offset that the
       // trajectory was added to a running simulation
-      true_pos.push_back(trajectories_.at(i)
-                             .GetState(curr_index_ - traj_index_offset_.at(i))
-                             .head<2>());
+      true_pos.push_back(target_pos.head<2>());
+      curr_state_.truth.push_back(std::make_pair(i, target_pos));
 
-      // The trajectories position in the vector is its id
-      curr_state_.truth.push_back(std::make_pair(
-          i, trajectories_.at(i).GetState(curr_index_ -
-                                          traj_index_offset_.at(i))));
+      // Update the cartesian position: zck = H*xk + sigma*rand
+      ObjectPosition2D temp =
+          (H_ * target_pos +
+           cartesian_std_dev_ *
+               utils::StdNormalGenerator<ScalarType, 2>().sample())
+              .eval();
 
-      // Update the cartesian position
-      cart_positions_.push_back(
-          trajectories_.at(i)
-              .GetState(curr_index_ - traj_index_offset_.at(i))
-              .head<2>());
+      cart_positions_.push_back(temp);
+      // Also update the current state
+      curr_state_.sensor.zx.push_back(temp(0));
+      curr_state_.sensor.zy.push_back(temp(1));
+
       // update the rang and azimuth states
+      // zpk = ((sqrt((xk - xs)^2 + (yk - ys)^2), arctan(yk - ys/xk - xs)) +
+      // noise
+      ObjectPosition2D temp2;
+
+      temp2 << sqrt((target_pos(0) - radar_position_.x) *
+                        (target_pos(0) - radar_position_.x) +
+                    (target_pos(1) - radar_position_.y) *
+                        (target_pos(1) - radar_position_.y)),
+          std::atan2((target_pos(1) - radar_position_.y),
+                     (target_pos(0) - radar_position_.x));
+
+      temp2 += Eigen::Matrix<ScalarType, 2, 1>(
+          range_std_dev_ *
+              utils::StdNormalGenerator<ScalarType, 1>().sample_raw(),
+          azimuth_std_dev_ *
+              utils::StdNormalGenerator<ScalarType, 1>().sample_raw());
+
+      rang_azimuth_states_.push_back(temp2);
+      curr_state_.sensor.range.push_back(temp2(0));
+      curr_state_.sensor.azimuth.push_back(temp2(1));
 
       has_update_ = true;  // Set the update flag to true
     }
