4a815528878924809d05a44dabab4cfeea432084
SPauly <spau@hotmail.de>
2025-06-01 12:09:03 +0200

    Status

---
+++ include/sensfus/kalman/kalman_filter.h
@@ -0,0 +1,17 @@
+#ifndef SENSFUS_KALMAN_KALMAN_FILTER_H
+#define SENSFUS_KALMAN_KALMAN_FILTER_H
+
+namespace sensfus {
+
+namespace kalman {
+
+class KalmanFilter {
+ public:
+  explicit KalmanFilter();
+};
+
+}  // namespace kalman
+
+}  // namespace sensfus
+
+#endif  // SENSFUS_KALMAN_KALMAN_FILTER_H
